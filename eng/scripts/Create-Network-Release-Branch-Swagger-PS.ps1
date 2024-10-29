ec4b33ff60f1c7d1576d03af8685ca016fa31fc2
Prajjwal kamboj <kamboj.prjwl@outlook.com>
2024-10-29 18:43:22 +0530

    Random commit

--- eng/scripts/Create-Network-Release-Branch-Swagger-PS.ps1
+++ eng/scripts/Create-Network-Release-Branch-Swagger-PS.ps1
@@ -1,6 +1,9 @@
 # create_new_branch.ps1
 
+$date = Get-Date -Format "yyyy-MM-dd"
+$branchName = "network-release-$date"
+
 git config --global user.email "kamboj.prjwl@outlook.com"
 git config --global user.name "kamboj-prjjwl"
-git checkout -b release_2
-git push origin release_2
+git checkout -b $branchName
+git push origin $branchName
